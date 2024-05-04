3bd76b7e000534a4821691690423425e547d68fa
Rick Bosch <emerick.bosch@gmail.com>
2024-05-04 18:02:06 +0200

    Draw condition check

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import {Board, BoardState, BoardStatus, Player, PlayerO} from "./types"
+import { Board, BoardState, BoardStatus, Player, PlayerO } from "./types"
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
         // initialize a new game
-        if(request.method === "GET") {
+        if (request.method === "GET") {
             const board = getBoard()
             return new Response(JSON.stringify(board));
         }
 
         // send a move; get the next state
-        if(request.method === "POST") {
+        if (request.method === "POST") {
             const board = getBoard()
             return new Response(JSON.stringify(board));
         }
 
-        return new Response("unkown operation", {status: 404})
-	},
+        return new Response("unkown operation", { status: 404 })
+    },
 };
 
 export function getBoard(state: BoardState | undefined = Array(3 * 3).fill(null), status: BoardStatus = "active"): Board {
-    return  { size: 3, state, status }
+    return { size: 3, state, status }
 }
 
-export function playRandomMove({state}: Board): Board {
+export function playRandomMove({ state }: Board): Board {
     const possibleMoves = state.map((value, index) => {
         return value === null ? index : undefined
     }).filter((indexValue): indexValue is number => indexValue !== undefined)
-
     const move = Math.floor(Math.random() * possibleMoves.length)
-    const moveIndex = possibleMoves[move]
+
+    const moveBoardIndex = possibleMoves[move]
     const newState = [...state]
-    newState[moveIndex] = PlayerO
+    newState[moveBoardIndex] = PlayerO
 
     return getBoard(newState)
 }
 
-export function checkDrawCondition({state}: Board): boolean {
+export function checkDrawCondition({ state }: Board): boolean {
     const movesLeft = state.find(value => value == null)
     return movesLeft === undefined
 }
@@ -46,62 +46,61 @@ export function checkWinCondition(board: Board): Player | undefined {
     return checkRowWinCondition(board) ?? checkColumnWinCondition(board) ?? checkDiagonalWinCondition(board)
 }
 
-function checkRowWinCondition({state: data, size}: Board): Player | undefined {
-    for(let row = 0; row < size; row++) {
-        let player = data[row*size]
-        if(player === null) {
+function checkRowWinCondition({ state: data, size }: Board): Player | undefined {
+    for (let row = 0; row < size; row++) {
+        let player = data[row * size]
+        if (player === null) {
             continue
         }
 
-        for(let column = 1; column < size; column++) {
-            if(data[row*size + column] !== player) {
+        for (let column = 1; column < size; column++) {
+            if (data[row * size + column] !== player) {
                 break;
             }
 
-            if(column === size - 1) {
+            if (column === size - 1) {
                 return player
             }
         }
     }
 }
 
-function checkColumnWinCondition({state: data, size}: Board): Player | undefined {
-    for(let column = 0; column < size; column++) {
+function checkColumnWinCondition({ state: data, size }: Board): Player | undefined {
+    for (let column = 0; column < size; column++) {
         let player = data[column]
-        if(player === null) {
+        if (player === null) {
             continue
         }
 
-        for(let row = 1; row < size; row++) {
-            console.log({column, row, value: data[row*size + column]})
-            if(data[row*size + column] !== player) {
+        for (let row = 1; row < size; row++) {
+            if (data[row * size + column] !== player) {
                 break;
             }
 
-            if(row === size - 1) {
+            if (row === size - 1) {
                 return player
             }
         }
     }
 }
 
-function checkDiagonalWinCondition({state: data, size}: Board): Player | undefined {
+function checkDiagonalWinCondition({ state: data, size }: Board): Player | undefined {
     const middleIndex = (Math.floor(size / 2) * size) + (Math.floor(size / 2) + 1) - 1
     const middle = data[middleIndex]
 
-    if(middle === null) {
+    if (middle === null) {
         return undefined
     }
 
     let leftDiagonalValid = true;
     let rightDiagonalValid = true;
-    for(let i = 0; (i < size) && (leftDiagonalValid || rightDiagonalValid); i++) {
+    for (let i = 0; (i < size) && (leftDiagonalValid || rightDiagonalValid); i++) {
         const leftDiagonalValue = data[(size * i) + i]
         const rightDiagonalValue = data[(size * i) + (size - i) - 1]
-        if(leftDiagonalValue !== middle) {
+        if (leftDiagonalValue !== middle) {
             leftDiagonalValid = false
         }
-        if(rightDiagonalValue !== middle) {
+        if (rightDiagonalValue !== middle) {
             rightDiagonalValid = false
         }
     }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,21 +1,21 @@
 import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import {checkWinCondition, getBoard, playRandomMove} from "../src"
-import {PlayerX,PlayerO} from "../src/types"
+import { checkDrawCondition, checkWinCondition, getBoard, playRandomMove } from "../src"
+import { PlayerX, PlayerO } from "../src/types"
 
 describe('tic-tac-toe worker', () => {
-	it('responds with an empty game state on fetch', async () => {
+    it('responds with an empty game state on fetch', async () => {
         const expected = JSON.stringify(getBoard())
 
-		const response = await SELF.fetch('https://haha.com');
-		expect(await response.text()).toEqual(expected);
-	});
+        const response = await SELF.fetch('https://haha.com');
+        expect(await response.text()).toEqual(expected);
+    });
 
     describe("playing moves", () => {
         it("will play random moves", () => {
             const moves = 3
             let board = getBoard();
-            for(let i = 0; i < moves; i++) {
+            for (let i = 0; i < moves; i++) {
                 board = playRandomMove(board)
             }
 
@@ -27,12 +27,41 @@ describe('tic-tac-toe worker', () => {
         })
     })
 
+    it("can check draw conditions", () => {
+        expect(checkDrawCondition(getBoard(
+            [
+                null, null, null,
+                null, null, null,
+                PlayerX, PlayerX, null
+            ]
+        ))
+        ).toEqual(false)
+
+        expect(checkDrawCondition(getBoard(
+            [
+                PlayerO, PlayerO, PlayerX,
+                PlayerX, null, PlayerO,
+                PlayerX, PlayerX, PlayerO
+            ]
+        ))
+        ).toEqual(false)
+
+        expect(checkDrawCondition(getBoard(
+            [
+                PlayerO, PlayerO, PlayerX,
+                PlayerX, PlayerO, PlayerO,
+                PlayerX, PlayerX, PlayerO
+            ]
+        ))
+        ).toEqual(true)
+    })
+
     describe("win condition checking", () => {
         it("returns the winning player for a row win", () => {
             const board = getBoard(
                 [
-                    null , null, null,
-                    null, null , null,
+                    null, null, null,
+                    null, null, null,
                     PlayerX, PlayerX, PlayerX
                 ]
             )
@@ -42,8 +71,8 @@ describe('tic-tac-toe worker', () => {
         it("returns the winning player for a column win", () => {
             const board = getBoard(
                 [
-                    null , null, PlayerO,
-                    null, null , PlayerO,
+                    null, null, PlayerO,
+                    null, null, PlayerO,
                     null, null, PlayerO
                 ]
             )
